a55d84a127c8208875c21b71ea310b9252dc84f4
sahibibadov <bdvshb@gmail.com>
2023-07-05 16:58:06 +0400

    fixed:autho compelted input

--- src/validation/authvalidation.js
+++ src/validation/authvalidation.js
@@ -2,7 +2,9 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 // phone regex
-const phoneRegExp = `^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$`;
+const phoneRegExp = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+const emailRegex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const schema = yup
   .object()
@@ -15,6 +17,7 @@ const schema = yup
     email: yup
       .string()
       .email("Invalid email format")
+      .matches(emailRegex, "Invalid email format")
       .required("Mail is required"),
     phoneNumber: yup
       .string()
