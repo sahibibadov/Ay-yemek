1701de02fbf07a65e611f9507e5a9fbbc894060d
sahibibadov <bdvshb@gmail.com>
2023-07-28 15:52:32 +0400

    fixed: accordion size,payment mobile page

--- src/i18n.js
+++ src/i18n.js
@@ -1,21 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from "i18next-http-backend"; // backendden dil json datasin getirmek
+import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector"; //
-
-/* //backendden data lamayacayiqsa bununla edirik localda
-const resources = {
-  az: {
-    translation: {
-      welcome: "xos geldiniz",
-    },
-  },
-  en: {
-    translation: {
-      welcome: "hello",
-    },
-  },
-}; */
 
 i18n
   .use(initReactI18next) // butun componentleri ucun el catan edir
@@ -24,8 +10,6 @@ i18n
   .init({
     //  lng: "az",
     fallbackLng: "az", // userin dili yoxdursa default acilacaq dili gosterir
-    //  lng: "az",// LanguageDetector istifade edersek yazilmir, lng yazilanda localdaki dili deyisir lng-dekin edir
-    //   resources, //localda etmek ucun
   });
 
 export default i18n;
