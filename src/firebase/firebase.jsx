f4dffe9c30cfb497dc7d0e6421ee249729b130ad
sahibibadov <bdvshb@gmail.com>
2023-08-29 19:19:51 +0400

    firabse config

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_API_KEY,
-  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_APP_ID,
-  databaseURL: import.meta.env.VITE_APP_DATABASE_URL,
+  apiKey: "AIzaSyANupwn9blin8bpLoCwLmGxRjTeIAqz4Vs",
+  authDomain: "fir-1-f62aa.firebaseapp.com",
+  projectId: "fir-1-f62aa",
+  storageBucket: "fir-1-f62aa.appspot.com",
+  messagingSenderId: "648867939574",
+  appId: "1:648867939574:web:d3eb21cb440a940d7158d7",
+  databaseURL: "https://fir-1-f62aa-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
